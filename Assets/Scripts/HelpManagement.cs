30b5956dc7670c609639821b87caeb1321f870b3
Leonard Seymore <leonardseymore@gmail.com>
2017-02-16 22:24:26 +0200

    Help

---
+++ Assets/Scripts/HelpManagement.cs
@@ -0,0 +1,23 @@
+﻿using BitAura;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public partial class Game : MonoBehaviour {
+
+    
+
+    
+    
+    IEnumerator HelpMe()
+    {
+        Card card = GetBestCardToPlay(CurrentPlayerCards);
+        if (card == null)
+        {
+
+        }
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/WindowManagement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public partial class Game : MonoBehaviour {
+    public GameObject SpellWindow;
+    public GameObject PotionWindow;
+    public GameObject InfoWindow;
+
+    public void ToggleSpellWindow(bool visible)
+    {
+        Camera.main.GetComponent<BlurOptimized>().enabled = visible;
+        SpellWindow.SetActive(visible);
+    }
+
+    public void TogglePotionWindow(bool visible)
+    {
+        Camera.main.GetComponent<BlurOptimized>().enabled = visible;
+        PotionWindow.SetActive(visible);
+    }
+
+    public void ToggleInfoWindow(bool visible)
+    {
+        Camera.main.GetComponent<BlurOptimized>().enabled = visible;
+        InfoWindow.SetActive(visible);
+    }
+}
